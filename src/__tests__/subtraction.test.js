523aae7e00bc16da4ef77faf3367b9299d26f32d
Ryan Pedersen <ryanpedersen@Ryans-MacBook-Pro-2.local>
2020-07-09 15:06:34 -0400

    subtraction testing

--- src/__tests__/subtraction.test.js
+++ src/__tests__/subtraction.test.js
@@ -5,11 +5,11 @@ describe('Subtraction testing', () => {
         expect(subtract(4, 1)).toBe(3);
       });
 
-      test('4 - 1 = 3', () => {
-        expect(subtract(4, 1)).toBe(3);
+      test('6 - 1 = 5', () => {
+        expect(subtract(6, 1)).toBe(5);
       });
 
-      test('4 - 1 = 3', () => {
-        expect(subtract(4, 1)).toBe(3);
+      test('10 - 1 = 9', () => {
+        expect(subtract(10, 1)).toBe(9);
       });
 })
