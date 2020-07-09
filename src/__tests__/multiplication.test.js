78df6baab5a19436fff952585dcd96d8e11baf95
Ryan Pedersen <ryanpedersen@Ryans-MacBook-Pro-2.local>
2020-07-09 15:05:01 -0400

    fix mult test

--- src/__tests__/multiplication.test.js
+++ src/__tests__/multiplication.test.js
@@ -1,15 +1,15 @@
 import { multiply } from '../math';
 
 describe('Multiplication testing', () => {      
-    test('3 * 3 = 9', () => {
-    expect(multiply(3, 3)).toBe(9);
+    test('3 * 1 = 3', () => {
+    expect(multiply(3, 1)).toBe(3);
     });
 
     test('3 * 3 = 9', () => {
         expect(multiply(3, 3)).toBe(9);
     });
 
-    test('3 * 3 = 9', () => {
+    test('3 * 5 = 15', () => {
         expect(multiply(3, 5)).toBe(15);
     });
                 
