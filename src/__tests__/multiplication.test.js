259cfa21ecf5da61b3a3588a572d593d68eba47b
zmarkan <zan@circleci.com>
2021-01-29 12:19:24 +0000

    add couple of failing tests

--- src/__tests__/multiplication.test.js
+++ src/__tests__/multiplication.test.js
@@ -13,4 +13,16 @@ describe('Multiplication testing', () => {
         expect(multiply(3, 5)).toBe(15);
     });
                 
+
+    test('3 * 5 = 15', () => {
+        expect(multiply(3, 5)).toBe(15);
+    });
+
+    test('3 * 5 = 16 should fail', () => {
+        expect(multiply(3, 5)).toBe(16);
+    });
+
+    test('3 * 5 = 17 should fail', () => {
+        expect(multiply(3, 5)).toBe(17);
+    });
 })
